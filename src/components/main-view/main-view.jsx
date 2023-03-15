804472339eda2897bb0fc6e9c9dacba61e3127ed
Nat-crit20 <greuelnathan74@gmail.com>
2023-03-15 11:17:03 -0500

    Made the code prettier

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,37 +1,63 @@
-import { useEffect, useState } from "react"
-import { MovieCard } from "../movie-card/movie-card"
-import { MovieView } from "../movie-view/movie-view"
+import { useEffect, useState } from "react";
+import { MovieCard } from "../movie-card/movie-card";
+import { MovieView } from "../movie-view/movie-view";
 
-export const MainView = () =>{
-    const [movies, setMovies] = useState([])
+export const MainView = () => {
+  const [movies, setMovies] = useState([]);
 
-    const [selectedMovie, setSelectedMovie] = useState(null)
-    useEffect(()=>{
-      fetch("https://blooming-shore-67354.herokuapp.com/movies").then(res=>res.json()).then(data=>{
-        setMovies(data)
-      })
-    })
-    if(selectedMovie){
-      let similarMovies = movies.filter(movie=> movie.Title !== selectedMovie.Title && movie.Genre.Name === selectedMovie.Genre.Name);
-      return (<div>
-                <MovieView movie={selectedMovie} onBackClick={()=>setSelectedMovie(null)} />
-                <br />
-                <h2>Similar Movies</h2>
-                {similarMovies.map((movie)=>{
-                  return <MovieCard movie={movie} key={movie._id} handleClick={(newSelectedMovie)=> {setSelectedMovie(newSelectedMovie)}} />
-                })}
-              </div>
-              )
-    }
-    if(movies.length === 0){
-        return <h1>There are no movies in the list</h1>
-    }
-
+  const [selectedMovie, setSelectedMovie] = useState(null);
+  useEffect(() => {
+    fetch("https://blooming-shore-67354.herokuapp.com/movies")
+      .then((res) => res.json())
+      .then((data) => {
+        setMovies(data);
+      });
+  });
+  if (selectedMovie) {
+    let similarMovies = movies.filter(
+      (movie) =>
+        movie.Title !== selectedMovie.Title &&
+        movie.Genre.Name === selectedMovie.Genre.Name
+    );
     return (
-        <div>
-            {movies.map(movie=>{
-                return <MovieCard movie={movie} key={movie._id} handleClick={(newSelectedMovie)=> {setSelectedMovie(newSelectedMovie)}} />
-            })}
-        </div>
-    )
-}
+      <div>
+        <MovieView
+          movie={selectedMovie}
+          onBackClick={() => setSelectedMovie(null)}
+        />
+        <br />
+        <h2>Similar Movies</h2>
+        {similarMovies.map((movie) => {
+          return (
+            <MovieCard
+              movie={movie}
+              key={movie._id}
+              handleClick={(newSelectedMovie) => {
+                setSelectedMovie(newSelectedMovie);
+              }}
+            />
+          );
+        })}
+      </div>
+    );
+  }
+  if (movies.length === 0) {
+    return <h1>There are no movies in the list</h1>;
+  }
+
+  return (
+    <div>
+      {movies.map((movie) => {
+        return (
+          <MovieCard
+            movie={movie}
+            key={movie._id}
+            handleClick={(newSelectedMovie) => {
+              setSelectedMovie(newSelectedMovie);
+            }}
+          />
+        );
+      })}
+    </div>
+  );
+};
