7f6a17c5b14ad13f3ed5d6d174ed5f1265090544
Nat-crit20 <greuelnathan74@gmail.com>
2023-03-15 11:23:45 -0500

    Created login form

---
+++ src/components/login-view/login-view.jsx
@@ -0,0 +1,13 @@
+export const LoginView = () => {
+  return (
+    <form action="">
+      <label htmlFor="">
+        Username: <input type="text" />
+      </label>
+      <label htmlFor="">
+        Password: <input type="password" />
+      </label>
+      <button type="submit">Submit</button>
+    </form>
+  );
+};
