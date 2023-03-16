74761ae546828da258041e9b6d404a33fa1f4747
Nat-crit20 <greuelnathan74@gmail.com>
2023-03-15 20:32:59 -0500

    Added handleSubmit func and useStates

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -1,11 +1,39 @@
+import { useEffect, useState } from "react";
+
 export const LoginView = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = () => {
+    const data = {
+      Username: username,
+      Password: password,
+    };
+    fetch("https://blooming-shore-67354.herokuapp.com/login", {
+      method: POST,
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+  };
   return (
-    <form action="">
+    <form action="" onSubmit={handleSubmit}>
       <label htmlFor="">
-        Username: <input type="text" />
+        Username:{" "}
+        <input
+          type="text"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
       </label>
       <label htmlFor="">
-        Password: <input type="password" />
+        Password:{" "}
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
       </label>
       <button type="submit">Submit</button>
     </form>
