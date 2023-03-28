1cab73e3f6bb450cb3f154f37d883b7ca28bdcb3
Nat-crit20 <greuelnathan74@gmail.com>
2023-03-28 18:45:10 -0500

    Added icons

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,6 +1,7 @@
 import PropTypes from "prop-types";
 import { Button, Card } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import { AiOutlineHeart, AiFillHeart } from "react-icons/ai";
 export const MovieCard = ({ movie }) => {
   return (
     <Card className="h-100">
@@ -8,6 +9,8 @@ export const MovieCard = ({ movie }) => {
       <Card.Body>
         <Card.Title>{movie.Title}</Card.Title>
         <Card.Text>{movie.Director.Name}</Card.Text>
+        <AiOutlineHeart style={{ color: "red", fontSize: "50px" }} />
+        <AiFillHeart style={{ color: "red", fontSize: "50px" }} />
         <Link to={`/movies/${encodeURIComponent(movie._id)}`}>
           <Button variant="link">Open</Button>
         </Link>
